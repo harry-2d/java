15fbf84addf92902238722410e82b2d35007e73f
harry-2d <51414908+harry-2d@users.noreply.github.com>
2019-08-19 12:59:58 +0545

    ok

--- Script.js
+++ Script.js
@@ -22,3 +22,51 @@ function multitable (){
     }
 
 }
+
+function currentDate(){
+   var d=new Date()
+   var day=d.getDay();
+   var date=d.getDate();
+   var month=d.getMonth();
+   var year=d.getFullYear();
+
+    if(day==0) day="Sunday";
+    else if(day==1) day="Monday";
+    else if(day==2) day="Tuesday";
+    else if(day==3) day="Wednesday";
+    else if(day==4) day="Thursday";
+    else if(day==5) day="Friday";
+    else day="Saturday";
+
+    if(month==0) month="Janurary";
+    else if(month==1) month="Feburary";
+    else if(month==2) month="March";
+    else if(month==3) month="April";
+    else if(month==4) month="May";
+    else if(month==5) month="June";
+    else if(month==6) month="July";
+    else if(month==7) month="August";
+    else if(month==8) month="September";
+    else if(month==9) month="October";
+    else if(month==10) month="November";
+    else month="December";
+    document.getElementById('currentDate').innerHTML+=day+ ","+""+ date+""+ month+""+ year
+
+    refreshTime();
+}
+
+function currentTime(){
+    var t = new Date();
+    var hh = t.getHours();
+    var mm = t.getMinutes();
+    var ss = t.getSeconds();
+    var ampm=hh<=11?"AM":"PM";
+    document.getElementById('hh').innerHTML=hh;
+    document.getElementById('mm').innerHTML=mm;
+    document.getElementById('ss').innerHTML=ss +" "+ampm ;
+}   
+
+function refreshTime(){
+    setInterval(currentTime, 1000);
+
+}
