a5ed2d63e49d2a77d6f1657a8b50a33c1b67cd0a
harry-2d <51414908+harry-2d@users.noreply.github.com>
2019-08-22 10:35:00 +0545

    update

--- validations.js
+++ validations.js
@@ -1,4 +1,12 @@
 function validations(){
-    alert("hi");
+    var username=document.getElementById("e");
+    var password=document.getElementById("p");
+    
+    if(username=="" && password=="")
+    {
+        document.getElementById('error').innerHTML="username and password is invalid";
+        return false;
+    }
+    alert(hello);
     return false;
 }
